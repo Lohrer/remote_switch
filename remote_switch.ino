30c84b78b49d7b9d83b098b7250b4fc21db78c25
Michael Lohrer <michael.f.lohrer@gmail.com>
2017-11-07 18:58:48 -0500

    Started request parsing

--- remote_switch.ino
+++ remote_switch.ino
@@ -21,10 +21,10 @@
 int pin = 0;
 bool switchOn = false;
 
-// Set the MAC address to an unused RHK address
+// Set the MAC address to what is printed on the shield
 // Set the IP address to 10.0.0.4
 byte mac[] = {
-  0x00, 0xD0, 0x0B, 0x00, 0x0F, 0xFF
+  0x90, 0xA2, 0xDA, 0x0F, 0x27, 0x38
 };
 IPAddress ip(10, 0, 0, 4);
 
@@ -35,7 +35,21 @@ enum states {
   GET,
   WAITFOREND,
   RESPONSE
-} state = GET;
+} state;
+
+// Start of GET line for all available requests,
+// these strings must be the same length
+char* GET_INDEX = "GET / HT";
+char* GET_ON    = "GET /on ";
+char* GET_OFF   = "GET /off";
+char* REQUEST_LINES[] = {GET_INDEX, GET_ON, GET_OFF};
+int REQUEST_LEN = strlen(GET_INDEX);
+enum requests {
+  INDEX,
+  ON,
+  OFF,
+  NONE
+} request;
 
 void setup() {
   // Open serial communications and wait for port to open:
@@ -52,43 +66,74 @@ void setup() {
 }
 
 void loop() {
+  int linecnt = 0;
+  
   // Listen for incoming clients
   EthernetClient client = server.available();
   if (client) {
     Serial.println("new client");
-    // An http request ends with a blank line
-    boolean currentLineIsBlank = true;
+
     state = GET;
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
         Serial.write(c);
+
+        switch (state) {
+          case GET:
+          {
+            int num_requests = sizeof(REQUEST_LINES) / sizeof(char*);
+
+            // Increment counter if it matches one of the supported requests.
+            request = NONE;
+            for (int i = 0; i < num_requests; i++) {
+              if (c == REQUEST_LINES[i][linecnt]) {
+                linecnt++;
+                request = (requests)i;
+                break;
+              }
+            }
+
+            // If no matching request line was found reject the request.
+            if (request == NONE) {
+              state = WAITFOREND;
+              Serial.println("invalid request");
+              break;
+            }
+
+            // If at the end of the request strings, update state
+            if (linecnt >= REQUEST_LEN-1) {
+              //Serial.printf("Request %d\n", (int)request);
+            }
+            break;
+          }
+          case WAITFOREND:
+            
+            break;
+          case RESPONSE:
+            
+            break;
+        }
         // If you've gotten to the end of the line (received a newline
         // character) and the line is blank, the http request has ended,
         // so you can send a reply
-        if (c == '\n' && currentLineIsBlank) {
-          // Send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // The connection will be closed after completion of the response
-          client.println();
-          client.println("<!DOCTYPE HTML><html>");
-          if (switchOn) {
-            client.println("Switch ON");
-          } else {
-            client.println("Switch OFF");
-          }
-          client.println("</html>");
-          break;
-        }
-        if (c == '\n') {
-          // You're starting a new line
-          currentLineIsBlank = true;
-        }
-        else if (c != '\r') {
-          // You've gotten a character on the current line
-          currentLineIsBlank = false;
-        }
+//        if (c == '\n' && currentLineIsBlank) {
+//          // Send a standard http response header
+//          client.println("HTTP/1.1 200 OK");
+//          client.println("Content-Type: text/html");
+//          client.println("Connection: close");  // The connection will be closed after completion of the response
+//          client.println();
+//          client.println("<!DOCTYPE HTML><html>");
+//          if (switchOn) {
+//            client.println("Switch ON");
+//          } else {
+//            client.println("Switch OFF");
+//          }
+//          client.println("</html>");
+//          break;
+//        }
+
+        
       }
     }
     // Give the web browser time to receive the data
